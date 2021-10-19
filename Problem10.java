dda37c42b4bf04b45a1df0c25a8c215a140a87bc
Rami Shafikov <rami@shafikov.ca>
2021-10-19 09:58:37 -0400

    Problems 10, 11

---
+++ Problem10.java
@@ -0,0 +1,26 @@
+/*	Rami Shafikov
+	Euler Problem #10
+	October 19 2021		*/
+
+public class Problem10 {
+	public static void main(String[] args) {
+		boolean prime;
+		long sumOfPrimes = 0;
+		for (int i=2; i<2000000; i++) {
+			prime = isPrime(i);
+			if (prime == true) {
+				sumOfPrimes += i;
+			}
+		}
+		System.out.println(sumOfPrimes);
+	}
+
+	static boolean isPrime(int i) {
+		for (int j=2; j<=(int) (Math.sqrt(i)); j++) {
+			if (i % j == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
